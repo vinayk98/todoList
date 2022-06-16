c0b895db08589972c197d57ac0c6f6ba2182151b
Jalaj Goyal <jalaj.goyal@sunstone.edu.in>
2022-06-16 17:39:31 +0530

    init commit

---
+++ utils/utils.js
@@ -0,0 +1,13 @@
+// const getNewData = (currentTab, data) => {
+//   if (currentTab === "active") {
+//     return data.filter((el) => {
+//       return el.checked === false;
+//     });
+//   } else if (currentTab === "completed") {
+//     return data.filter((el) => {
+//       return el.checked === true;
+//     });
+//   } else if (currentTab === "all") {
+//     return data;
+//   }
+// };
